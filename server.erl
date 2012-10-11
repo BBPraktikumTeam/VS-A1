abeeb0456593b6096b9d5b326ce7e3747922777c
oliver <oliver.behncke@haw-hamburg.de>
2012-10-11 10:36:59 +0200

    commit for safety purposes :)

--- server.erl
+++ server.erl
@@ -1,6 +1,7 @@
 -module(server).
 -compile(export_all).
--record(state, {clients,message_id,messages,delivery_queue,holdback_queue}).
+-import(werkzeug).
+-record(state, {clients,message_id,delivery_queue,holdback_queue}).
 
 %%Client -> erhaltene Nachricht
 %Client -> Letze Meldung
@@ -9,56 +10,86 @@
 %Timeout
 
 
-loop(S= #state{message_id=Id,delivery_queue=DQ,messages = Messages}) ->
+loop(S= #state{message_id=Id,delivery_queue=DQ}) ->
 	    receive
 		{getmessages,Pid} ->
-		  {{_,Msg},NQ}=queue:out(DQ),
-		  Pid ! Msg,
-		  loop(S#state{delivery_queue=NQ});
+		  NewState=getmessages(Pid,S),
+		  loop(NewState);
 		{dropmessage, {Message,Number}} ->
-		  New_messages = append(Number,Message,Messages),
-		  NewState=dropmessage(S,{Message,Number}),
-		  io:format("~p :  ~s~n",[Number,Message]),
-		  loop(NewState});
+		  NewState=dropmessage({Message,Number},S),
+		  loop(NewState);
 		{getmsgid,Pid} -> 
 		  io:format("Send Id ~p~n",[Id]), 
 		  Pid ! {Id}, 
 		  loop(S#state{message_id=Id+1})
 	    end.
 
-dropmessage(S = #state{messages=Messages,delivery_queue = DQ, holdback_queue = HQ}, {Message,Number}) -> 
-	NewState = S#state
-		    {holdback_queue=queue:in(Number,HQ),
-		    messages=append(Number,Message,Messages),
-	organize_queues(NewState).
-      
-    
+   
 
-update_queues(S = #state{messages=Messages,delivery_queue = DQ, holdback_queue = HQ}, {Message,Number}}) -> 
-    AllowedMessage=lists:max(DQ)+1,
-    case lists:member(AllowedMessage, HQ) of
-      true -> 
-	%% Sorted List besser, damit out funzt
-	{{value,Msg},NewHQ} = queue:out(Number),
-	NewDQ = queue:in(Msg, DQ),
-	Length = queue:len(DQ),  %% Length of the List
-	if Length > 10 ->   %% MaxLength of DQ    
-	  {{value,Msg}, Queue} = queue:out(NewDQ),
-	  S#state
-	    {holdback_queue=NewHQ,
-	     delivery_queue=Queue,
-	   );
-	true ->
-	  S#state
-	    {holdback_queue=NewHQ,
-	     delivery_queue=NewDQ,
-	   )
-      false -> 
-    end.
+getmessages(Pid,S=#state{delivery_queue=DQ, clients = Clients}) -> 
+  MsgId=getLastMsgId(Pid,S),
+  case lists:dropwhile(fun({_,X})-> X<=MsgId end,DQ) of
+    [] ->
+      Message="Keine neuen Nachrichten vorhanden;",
+      Getall=true,
+      NewMsgId=MsgId;
+    [{Message,NewMsgId}] -> 
+      Getall = true;
+    [{Message,NewMsgId}|_] ->
+      Getall = false
+   end,
+   Pid ! {appendTimeStamp(Message,"Sendezeit"),Getall},
+   S#state{clients=orddict:store(Pid,{NewMsgId,timestamp()})}.
 
 
+appendTimeStamp(Message,Type) ->
+  Message++" "++Type++": "++werkzeug:timeMilliSecond().
 
-init() -> loop(#state{clients=orddict:new(),messages=orddict:new(),delivery_queue=queue:new(),message_id=1}).
+
+getLastMsgId(Pid,S=#state{clients = Clients}) ->
+    %prüfen ob Client bereits bekannt:
+  case orddict:find(Pid,Clients) of
+      error ->
+	orddict:append(Pid,{1,timestamp()}, Clients);
+	1.
+      {ok,{MsgId,_}} -> MsgId.
+
+dropmessage({Message,Number},S=#state{holdback_queue=HQ}) ->
+  % hier könnte ein Fehler geschmissen werden, wenn schon eine Nachricht mit der ID vorhanden ist, momentan wird sie überschrieben
+  NewMessage=Message++"Empfangszeit: "++werkzeug:timeMilliSecond(),
+  %% Sorted Insert in the List
+  NewHQ=lists:takewhile(fun({_,X})-> X< Number end,HQ)++[{NewMessage,Number}]++lists:dropwhile(fun({_,X})-> X<Number end,HQ),
+  werkzeug:logging("NServer.log",NewMessage),
+  S#state{holdback_queue=NewHQ}.
+  
+
+%update_queues(S = #state{messages=Messages,delivery_queue = DQ, holdback_queue = HQ}, {Message,Number}}) -> 
+%    AllowedMessage=lists:max(DQ)+1,
+%    case lists:member(AllowedMessage, HQ) of
+%      true -> 
+%	%% Sorted List besser, damit out funzt
+%	{{value,Msg},NewHQ} = queue:out(Number),
+%	NewDQ = queue:in(Msg, DQ),
+%	Length = queue:len(DQ),  %% Length of the List
+%	if Length > 10 ->   %% MaxLength of DQ    
+%	  {{value,Msg}, Queue} = queue:out(NewDQ),
+%	  S#state
+%	    {holdback_queue=NewHQ,
+%	     delivery_queue=Queue,
+%	   );
+%	true ->
+%	  S#state
+%	    {holdback_queue=NewHQ,
+%	     delivery_queue=NewDQ,
+%	   )
+%      false -> 
+%    end.
+timestamp() -> 
+  {Mega, Secs, _} = now(),
+  Timestamp = Mega*1000000 + Secs.
+
+
+init() -> loop(#state{clients=orddict:new(),delivery_queue=[{"Nachricht1",1},{"Nachricht2",2}],message_id=1,holdback_queue=[]}).
 
 
 start() -> spawn(fun init/0).
