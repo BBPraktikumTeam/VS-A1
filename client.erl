6165929407fe911837ea56b06792c69aa738aa5c
kaib <kai.bielenberg@haw-hamburg.de>
2012-10-14 08:51:29 +0200

    Client extended

--- client.erl
+++ client.erl
@@ -1,3 +1,16 @@
 -module(client).
 -compile(export_all).
 -import(werkzeug).
+
+init() -> 
+	{ok, ConfigListe} = file:consult("client.cfg"),
+	{ok, ClientsNr} = get_config_value(clients, ConfigListe),
+	{ok, Lifetime} = get_config_value(lifetime, ConfigListe),
+	{ok, Servername} = get_config_value(servername, ConfigListe),
+	{ok, Sendeintervall} = get_config_value(sendeintervall, ConfigListe),
+	loop(now()).
+
+
+start() -> spawn(fun init/0).
+
+loop(StartTime) -> 
